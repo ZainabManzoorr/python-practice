7046bed34265e6eda178760db51a05f19d61248e
Zainab Manzoor <zainabmanzoorzoor@gmail.com>
2025-10-24 21:51:49 +0500

    Practiced math operations with math library

---
+++ math_1.py
@@ -0,0 +1,15 @@
+import math
+
+print(5/2) # Floating-point division
+print(5//2) # Floor division
+print(-3//2) # Floor division with negative number
+print(int(-3/2)) # Converting float division to int
+
+print(10 % 3) # Modulus operator
+print(-10 % 3) # Modulus with negative number
+
+print(math.fmod(-10, 3)) # Using math.fmod for modulus
+print(math.floor(-10 / 3)) # Using math.floor for floor division
+print(math.ceil(10 / 3)) # Using math.ceil for ceiling division
+print(math.sqrt(16)) # Square root
+print(math.pow(2, 3)) # Power function
