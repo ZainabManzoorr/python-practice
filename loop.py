be3d8aad40f5ff2f623efcc3d3fe0e93b10c9ed2
Zainab Manzoor <zainabmanzoorzoor@gmail.com>
2025-10-24 21:42:39 +0500

    Practiced for loop with increment and decrement

---
+++ loop.py
@@ -0,0 +1,8 @@
+for i in range(5):
+    print("Iteration:", i)
+for i in range(2,6):
+    print("Iteration from 2 to 5:", i)
+for i in range(6,2,-1):
+    print("Reverse Iteration from 5 to 3:", i)
+for i in range(10,1,-2):
+  print("Reverse Iteration from 10 to 2 with step 2:", i)
